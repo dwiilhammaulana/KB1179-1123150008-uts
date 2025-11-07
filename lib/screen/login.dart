71f6bc15feeaa7dbc2f5e5cc86d16272c76765c2
dwiilhammaulana <1123150008@global.ac.id>
2025-11-07 23:27:25 +0700

    membuat button login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -93,6 +93,33 @@ class _LoginState extends State<Login> {
                     ),
                   ),
                 ),
+                const SizedBox(height: 30),
+                Container(
+
+                  child: SizedBox(
+                    height: 40,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 14,
+                          horizontal: 20,
+                        ),
+                      ),
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
