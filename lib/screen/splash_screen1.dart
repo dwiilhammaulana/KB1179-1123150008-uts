04593ffea2663e407a90e8d778a57dbe72901174
dwiilhammaulana <1123150008@global.ac.id>
2025-11-07 20:33:04 +0700

    membuat circle container

--- lib/screen/splash_screen1.dart
+++ lib/screen/splash_screen1.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
-class  Screen1 extends StatelessWidget {
-  const Screen1 ({super.key});
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("1123150008"),
+        actions: const [],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 25),
+            Container(
+              height: 200,
+              width: 200,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
